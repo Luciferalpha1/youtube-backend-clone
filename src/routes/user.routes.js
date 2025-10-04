3509925fe23dd148748e2dd112346a802aa94b08
Luciferalpha1 <87848730+Luciferalpha1@users.noreply.github.com>
2025-10-04 15:17:23 +0530

    added respective routes for controllers.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -34,7 +41,27 @@ router.route("/login").post(loginUser);
 
 //secured routes:
 //Logout Route (here verifyJWT is a middleware.)
+// using post as you are sending info
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+//using get as you are retrieving data
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+//using patch, to apply minimial/selective changes to a resource.
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+//here upload.single was used as only a single file is being handled.
+router
+  .route("/change-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/change-coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+// /c/:username is the parameter, as we used req.params in the controller.
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 
 export default router; // can be imported using any name.
