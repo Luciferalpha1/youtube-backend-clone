e8adef757b0953945953670ba63b3afb5f6e02a8
Luciferalpha1 <87848730+Luciferalpha1@users.noreply.github.com>
2025-10-05 20:36:56 +0530

    added like, playlist, tweet, comment models.

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    //comment will be on a single video.
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    //User
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+export const comment = mongoose.model("Comment", commentSchema);
---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    //array of all videos
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    //User
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const playlist = new mongoose.model("Playlist", playlistSchema);
